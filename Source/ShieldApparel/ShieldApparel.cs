772098af8bd2588d679c0e9fc33bb35b08827d56
Yakhmet <Yasinahmetakson@gmail.com>
2024-04-08 23:09:18 +0300

    Shield Visibility Fix (Hide on Undraft)
    Uses Harmony. Re-renders equipped apparels each second, checks for Draft. If not drafted, doesn't render apparels in the category of GW_Shield.

---
+++ Source/ShieldApparel/ShieldApparel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using UnityEngine;
+using HarmonyLib;
+using Verse.AI;
+
+namespace GrimWorld
+{
+    [StaticConstructorOnStartup]
+    public static class Start
+    {
+
+        static Start()
+        {
+            var harmony = new Harmony("com.example.patch");
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(Pawn_DraftController), nameof(Pawn_DraftController.DraftControllerTick))]
+    public static class OnPawnDraftRenderer {
+        public static int draftShieldStateUpdateCounter = 0;
+        public static void Postfix(Pawn_DraftController __instance) {
+            draftShieldStateUpdateCounter++;
+
+            if(draftShieldStateUpdateCounter >= 60) { 
+                __instance.pawn.apparel.Notify_ApparelChanged();
+                draftShieldStateUpdateCounter = 0;
+
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(ApparelGraphicRecordGetter), nameof(ApparelGraphicRecordGetter.TryGetGraphicApparel))]
+    public static class ShieldNoRenderRules
+    {
+        public static bool Prefix(Apparel apparel, BodyTypeDef bodyType, out ApparelGraphicRecord rec)
+        {
+            if (apparel.HasThingCategory(DefDatabase<ThingCategoryDef>.GetNamed("GW_Shield")))
+            {
+                if (!apparel.Wearer.Drafted) {
+                    rec = new ApparelGraphicRecord();
+                    return false;
+                }
+            }
+            
+            rec = new ApparelGraphicRecord(null, null);
+            return true;
+        }
+    }
+}
